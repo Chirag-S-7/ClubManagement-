5518f386356e3da66dcaa54e0c94b5d3b822faf6
Chirag Shah <chiragladdha9104@gmail.com>
2022-03-24 19:54:17 +0530

    24-3

--- ClubManagement/ClubManagement/settings.py
+++ ClubManagement/ClubManagement/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d=-j6yuww6nf4i++^8dj@kw$6o0#v9nf#%gw32y*f!sk09=q_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- ClubManagement/ClubManagement/urls.py
+++ ClubManagement/ClubManagement/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls.static import static
 # from django.conf.urls import url
 
 admin.site.site_header = "YourEvent Admin"
@@ -23,5 +25,7 @@ admin.site.index_title = "Welcome to YourEvent Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clubM.urls'))
+    path('', include('clubM.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
